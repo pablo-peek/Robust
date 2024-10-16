f7bad654307f2b3077784f0cb47c42d0f81ee6b3
pablo-peek <pablovargas72h@gmail.com>
2024-10-15 20:11:01 -0600

    Refactor app.js to add a root route and improve error handling

--- src/app.js
+++ src/app.js
@@ -15,21 +15,36 @@ app.get('/health', (req, res) => {
   return res.sendStatus(200);
 });
 
-/** 
+app.get('/', (req, res) => {
+  const htmlResponse = `
+  <html>
+    <head>
+      <title>Api</title>
+    </head>
+    <body>
+        <h1>API</h1>
+      <p>Welcome to the API</p>
+    </body>
+  </html>
+`;
+  res.send(htmlResponse);
+});
+
+/**
  * Routers
-*/
+ */
 app.use('/api', router);
 
 app.use((error, req, res, next) => {
   const status = error.status;
-  res.status(status).json({ 
-    status, 
-    ...(error?.message && {errorData: error?.message}) 
+  res.status(status).json({
+    status,
+    ...(error?.message && { errorData: error?.message })
   });
 });
 
 app.use((req, res, next) => {
-  res.status(404).json({status: 404, errorData: 'Not Found' });
+  res.status(404).json({ status: 404, errorData: 'Not Found' });
 });
 
 module.exports = app;
