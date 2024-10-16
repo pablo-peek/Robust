44a1731e9fd5e2348c8b16bec84ada4813508f7b
Pablo Vargas <pablovargas72h@gmail.com>
2024-10-16 09:23:04 -0600

    Refactor database connection logic in index.js to remove unnecessary isConnected variable

--- src/models/index.js
+++ src/models/index.js
@@ -1,15 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-let isConnected = false;
-
 const connectDB = async () => {
-    if (isConnected) {
-        return;
-    }
     try {
         await mongoose.connect(process.env.ROBUST_MONGODB_URI, { connectTimeoutMS: 5000 });
-        isConnected = true;
         console.log('Database connected');
     } catch (error) {
         console.error('Error:', error);
