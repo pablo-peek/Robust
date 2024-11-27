045f6e1caa9477fc6ec0cf43ff7b51cea4a954ea
pablo-peek <pablovargas72h@gmail.com>
2024-11-27 04:53:36 -0600

    Refactor formatTime utility to use CommonJS module syntax

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,7 +1,9 @@
-export function formatTime(timeInSeconds) {
+function formatTime(timeInSeconds) {
     const minutes = Math.floor(timeInSeconds / 60);
     const seconds = Math.floor(timeInSeconds % 60);
     const milliseconds = Math.floor((timeInSeconds * 1000) % 1000);
 
     return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${milliseconds.toString().padStart(3, '0')}`;
 }
+
+module.exports = formatTime;
