dab0fa93aa33204403f7f0bce0076c93a0ec1669
pablo-peek <pablovargas72h@gmail.com>
2024-11-29 00:40:02 -0600

    Filter users in getAllUsers method to exclude those without a best time

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -6,37 +6,39 @@ require('dotenv').config();
 class UserService {
   constructor() {}
 
-  async getAllUsers(userId, raceNumber, page, limit) {
-      try {
-          const users = await User.aggregate([
-              { $match: { races: { $exists: true, $not: { $size: 0 } } } },
-              { $sort: { "races.bestTime": 1 } },
-              { $skip: (page - 1) * limit },
-              { $limit: limit },
-              { $project: {
-                  _id: 1,
-                  username: 1,
-                  races: { $arrayElemAt: ["$races", raceNumber - 1] }
-              }}
-          ]);
-  
-          const usersWithCurrentUserFlag = users.map(user => {
-              const race = user.races;
-              return {
-                  ...user,
-                  isCurrentUser: user._id.toString() === userId,
-                  races: race ? [{
-                      ...race,
-                      formattedBestTime: formatTime(race.bestTime)
-                  }] : []
-              };
-          });
-  
-          return usersWithCurrentUserFlag;
-      } catch (error) {
-          throw error;
-      }
-  }
+    async getAllUsers(userId, raceNumber, page, limit) {
+        try {
+            const users = await User.aggregate([
+                { $match: { races: { $exists: true, $not: { $size: 0 } } } },
+                { $sort: { "races.bestTime": 1 } },
+                { $skip: (page - 1) * limit },
+                { $limit: limit },
+                { $project: {
+                    _id: 1,
+                    username: 1,
+                    races: { $arrayElemAt: ["$races", raceNumber - 1] }
+                }}
+            ]);
+    
+            const filteredUsers = users.filter(user => user.races && user.races.bestTime !== undefined);
+    
+            const usersWithCurrentUserFlag = filteredUsers.map(user => {
+                const race = user.races;
+                return {
+                    ...user,
+                    isCurrentUser: user._id.toString() === userId,
+                    races: race ? [{
+                        ...race,
+                        formattedBestTime: formatTime(race.bestTime)
+                    }] : []
+                };
+            });
+    
+            return usersWithCurrentUserFlag;
+        } catch (error) {
+            throw error;
+        }
+    }
 }
 
 module.exports = UserService;
